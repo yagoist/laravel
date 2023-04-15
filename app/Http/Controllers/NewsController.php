31acfbf20e63e41d6e5a06df1f12b2fc2de0ec30
yagoist <87032238+yagoist@users.noreply.github.com>
2023-04-15 18:20:51 +0300

    HW3 blade

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -5,18 +5,21 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class NewsController extends Controller
 {
     use NewsTrait;
 
-    public function index() {
+    public function index(): View
+    {
         return \view('news.index', [
             'news' => $this->getNews()
         ]);
     }
 
-    public function show(int $id) {
+    public function show(int $id): View
+    {
         return \view('news.show', [
             'news' => $this->getNews($id)
         ]);
--- resources/views/News/index.blade.php
+++ resources/views/News/index.blade.php
@@ -1,12 +1,15 @@
-@foreach($news as  $n)
+@if (empty($news))
+    <h1>Новостей нет</h1>
+@else
+    @foreach($news as  $n)
     <div style="border: 1px solid black">
-        <h2><?= $n['title'] ?></h2>
-        <p><?=$n['description']?></p>
+        <h2> {{ $n['title'] }} </h2>
+        <p>{{ $n['description'] }}</p>
         <div>
-            <strong><?=$n['author']?> (<?= $n['created_at'] ?>)</strong>
-            <a href="<?=route('news.show', ['id' => $n['id']])?>">подробнее</a>
+            <strong> {{ $n['author'] }} ({{ $n['created_at'] }})</strong>
+            <a href="{{ route('news.show', ['id' => $n['id']]) }}">подробнее</a>
         </div>
     </div>
-
-@endforeach
+    @endforeach
+@endif
 
